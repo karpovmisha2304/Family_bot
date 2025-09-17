a2a1f382d5a55418febc0a1e9531bca69277d986
KARPOV MISHA <karpovmisha23041986@gmail.com>
2025-09-17 13:56:11 +0300

    3

--- handlers/user.py
+++ handlers/user.py
@@ -42,16 +42,26 @@ async def name_product(message: Message, state: FSMContext):
 @user_router.message(StateFilter(FSMFillForm.fill_price), lambda x: x.text.isdigit())
 async def price_purchases(message: Message, state: FSMContext):
     await state.update_data(price=message.text)
-    user_dict = await state.get_data()
-    print(user_dict)
-    print(user_dict['name'], user_dict['price'])
+    user_dict[message.from_user.id] = await state.get_data()
+    print(user_dict[message.from_user.id])
+    string = ' '.join(v for k, v in user_dict[message.from_user.id].items()) + '\n'
+    
+    with open ('C:/проекты_пайтон/Family_bot/purchases.txt', 'a', encoding='utf-8') as file:
+        file.write(string)
     await state.clear()
     await message.answer(text='Цена записана\n\nДля продолжения выберите команду в Меню')
     
-@user_router.message(Command(commands='view'), StateFilter(default_state))
+@user_router.message(Command(commands='view'))
 async def view_purchases(message: Message):
-    print(user_dict)
-    if user_dict:
-        await message.answer(user_dict['name'], user_dict['price'])
-    else:
-        await message.answer(text='Покупки еще не внесены\nВыберите дальнейшие действия в Меню')
+    with open ('C:/проекты_пайтон/Family_bot/purchases.txt', 'r', encoding='utf-8') as file:
+        rd = file.read()
+        print(rd, len(rd))
+        if len(rd) > 0:
+            await message.answer(text=f'{rd}')
+        else:
+            await message.answer(text='Покупки еще не внесены\nВыберите дальнейшие действия в Меню')
+            
+@user_router.message(Command(commands='del'))
+async def del_purchases(message: Message):
+    with open ('C:/проекты_пайтон/Family_bot/purchases.txt', 'w', encoding='utf-8') as file:
+        await message.answer(text=LEXICON_RU['/del'])
